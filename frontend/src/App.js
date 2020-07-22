e1f1fec7df8802c85f5e1a57cc7771d1d8c430f7
Mariany Ferreira <myf@certi.org.br>
2020-07-22 20:34:17 -0300

    feat: implement SPA and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import SeachPage from './pages/SearchPage';
+import UauPage from './pages/UauPage';
+
+import Header from './components/organisms/Header';
+import SPA from './components/templates/SPA';
+
 function App() {
   return (
-    <div>
-      <header>
-        <p>Mobiliza</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <SPA header={<Header />}>
+        <Route exact path="/">
+          <Redirect to="/search" />
+        </Route>
+        <Switch>
+          <Route path="/search" exact component={SeachPage} />
+          <Route path="/uau" exact component={UauPage} />
+          <Route render={() => <Redirect to="/search" />} />
+        </Switch>
+      </SPA>
+    </BrowserRouter>
   );
 }
 
