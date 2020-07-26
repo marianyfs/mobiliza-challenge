5e59bc84065ca45d969d6356a360e451c51b4cff
Mariany Ferreira <myf@certi.org.br>
2020-07-26 19:46:35 -0300

    refactor: SearchPage adding OMDb Movie API, Search, pagination and Items

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import {
+  Alert,
+} from 'react-bootstrap';
+
+import Loading from '../components/atoms/Loading';
+import ListItems from '../components/molecules/ListItems';
+import Pagination from '../components/molecules/Pagination';
+import Search from '../components/molecules/Search';
+
+import ItemsTemplate from '../components/templates/ItemsTemplate';
+
+import useSearch from './searchPage.utils';
 
 function SearchPage() {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+
+  const {
+    error,
+    items,
+    loading,
+    numberOfItems,
+  } = useSearch(query, page);
+
   return (
-    <p>SearchPage</p>
+    <ItemsTemplate
+      error={error
+        ? <Alert variant="primary">Erro ao efetuar a busca</Alert>
+        : undefined}
+      loading={loading
+        ? <Loading />
+        : undefined}
+      items={(
+        <ListItems
+          buttonLabel="UAU"
+          // Adicionar no Redux
+          handleItemClick={() => {}}
+          items={items}
+        />
+      )}
+      pagination={(
+        <Pagination
+          currentPage={page}
+          handlePageClick={setPage}
+          numberOfItems={Number(numberOfItems)}
+        />
+      )}
+      search={<Search handleSearch={setQuery} />}
+      title="Front-end dev Mobiliza - Movie App"
+    />
   );
 }
 
