f2ff743d0c5abdfe97511a9b91aed023d8b97e46
Mariany Ferreira <myf@certi.org.br>
2020-07-26 19:33:21 -0300

    feat: add Search component

---
+++ frontend/src/components/molecules/Search.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { func } from 'prop-types';
+
+import {
+  Button, FormControl, InputGroup,
+} from 'react-bootstrap';
+
+const Search = ({ handleSearch }) => {
+  const [search, setSearch] = useState('');
+
+  function handleSearchChange(event) {
+    event.preventDefault();
+    setSearch(event.target.value);
+  }
+
+  return (
+    <InputGroup className="mb-3">
+      <FormControl
+        onChange={handleSearchChange}
+        placeholder="Digite o termo para pesquisa"
+      />
+      <InputGroup.Append>
+        <Button
+          onClick={(event) => {
+            event.preventDefault();
+            handleSearch(search);
+          }}
+          type="submit"
+        >
+          Procurar
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+  );
+};
+
+Search.defaultProps = {
+  handleSearch: () => {},
+};
+
+Search.propTypes = {
+  handleSearch: func,
+};
+
+export default Search;
