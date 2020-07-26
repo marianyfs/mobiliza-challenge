5412e818b49e390a088943be7a005fa6252ab9fd
Mariany Ferreira <myf@certi.org.br>
2020-07-26 19:46:35 -0300

    refactor: SearchPage adding OMDb Movie API, Search, pagination and Items

---
+++ frontend/src/pages/searchPage.utils.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const search = (query, page) => {
+  let cancel = () => {};
+  return {
+    cancel,
+    get: {
+      // eslint-disable-next-line no-return-assign
+      cancelToken: new axios.CancelToken(((c) => (cancel = c))),
+      method: 'GET',
+      params: { s: query, page, apiKey: process.env.REACT_APP_KEY },
+      url: 'http://www.omdbapi.com/',
+    },
+  };
+};
+
+export default function useSearch(query, page) {
+  const [error, setError] = useState(false);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [numberOfItems, setNumberOfItems] = useState(0);
+
+  useEffect(() => {
+    setItems([]);
+  }, [query]);
+
+  useEffect(() => {
+    const request = search(query, page);
+
+    if (!query) {
+      return () => request.cancel();
+    }
+
+    setLoading(true);
+    setError(false);
+    axios(request.get)
+      .then((result) => {
+        const { data: { Search = [], totalResults = 0 } } = result;
+
+        setItems(Search);
+        setNumberOfItems(totalResults);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (!axios.isCancel(err)) {
+          setError(true);
+          setLoading(false);
+        }
+      });
+
+    return () => request.cancel();
+  }, [query, page]);
+
+  return {
+    error, items, loading, numberOfItems,
+  };
+}
