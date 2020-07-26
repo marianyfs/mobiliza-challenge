bfeed755e97998ea794d54bcd617f13f66280141
Mariany Ferreira <myf@certi.org.br>
2020-07-26 19:33:54 -0300

    style: apply alphabetic order

--- frontend/src/components/organisms/Header.js
+++ frontend/src/components/organisms/Header.js
@@ -6,6 +6,7 @@ import { Nav, Navbar } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 
 import Mobiliza from '../atoms/Mobiliza';
+
 import variables from '../../styles/_variables.scss';
 
 const Header = ({ location }) => (
@@ -13,15 +14,15 @@ const Header = ({ location }) => (
 
     <Navbar.Brand href="https://mobiliza.com.br/">
       <Mobiliza
-        fill={variables.primary}
         className="my-auto mx-2 my-auto"
+        fill={variables.primary}
         height={24}
       />
     </Navbar.Brand>
 
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="my-auto" activeKey={location.pathname}>
+      <Nav activeKey={location.pathname} className="my-auto">
         <Nav.Link className="text-uppercase" href="/search">Search</Nav.Link>
         <Nav.Link className="text-uppercase" href="/uau">UAU</Nav.Link>
       </Nav>
