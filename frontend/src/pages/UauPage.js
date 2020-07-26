1b57c46de9355cd0d7f839d0ca0f5a8fb348ec24
Mariany Ferreira <myf@certi.org.br>
2020-07-26 19:48:36 -0300

    refactor: UauPage adding movie items template

--- frontend/src/pages/UauPage.js
+++ frontend/src/pages/UauPage.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {
+  arrayOf, shape, string,
+} from 'prop-types';
 
-function UauPage() {
+import ListItems from '../components/molecules/ListItems';
+
+import ItemsTemplate from '../components/templates/ItemsTemplate';
+
+// preencher com o redux
+function UauPage({ items }) {
   return (
-    <p>UauPage</p>
+    <ItemsTemplate
+      title="Lista de filmes UAU"
+      items={(
+        <ListItems
+          buttonLabel="Remover"
+          // Remover do Redux
+          handleItemClick={() => {}}
+          items={items}
+        />
+      )}
+    />
   );
 }
+
+UauPage.defaultProps = {
+  items: [],
+};
+
+UauPage.propTypes = {
+  items: arrayOf(shape({
+    imdbID: string,
+    Title: string,
+  })),
+};
 
 export default UauPage;
